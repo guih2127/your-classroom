c660da550d22951141f2fed7163bbe4fc73f1727
Guilherme Reis Araujo <guih2127@gmail.com>
2019-04-17 01:38:32 -0300

    Criação das primeiras tabelas, EntityModel, além de Controller, View e Services das Classrooms

---
+++ YourClassroom/Controllers/ClassesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YourClassroom.Models;
+using YourClassroom.Services;
+using Microsoft.AspNet.Identity;
+
+namespace YourClassroom.Controllers
+{
+    public class ClassesController : Controller
+    {
+        private static ClasseService _classeService = new ClasseService();
+        private static RoleService _roleService = new RoleService();
+
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (_roleService.GetUserRoleById(User.Identity.GetUserId()) == "Professor")
+                {
+                    ViewBag.Classes = _classeService.ObterClassesPorIDProfessor(User.Identity.GetUserId());
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ YourClassroom/Services/ClasseService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YourClassroom.Models;
+using System.Data.Entity;
+
+namespace YourClassroom.Services
+{
+    public class ClasseService
+    {
+        YourClassroomEntities context;
+
+        public ClasseService()
+        {
+            context = new YourClassroomEntities();
+        }
+
+        public List<Classes> ObterClassesPorIDProfessor(string idProfessor)
+        {
+            List<Classes> classes = context.Classes.Where(c => c.ID_Professor == idProfessor).ToList();
+            return classes;
+        }
+    }
+}
